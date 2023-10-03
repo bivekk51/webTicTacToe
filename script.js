af85d1103a851887744e55b51892cfca6cbe8fd6
nepaldynasty09 <115235822+nepaldynasty09@users.noreply.github.com>
2023-10-03 11:22:52 +0545

    Update script.js
    Updated all the scripts and added a reset game function so that user could reset the scoreboard

--- script.js
+++ script.js
@@ -1,63 +1,132 @@
-let currentPlayer = 'X'; // X starts
-let board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-];
+let currentPlayer = "X";
+let playerOneWins=0;
+let playerTwoWins=0;
+let draw=0;
+let winner;
 
-function makeMove(row, col) {
-    if (board[row][col] === '') {
-        board[row][col] = currentPlayer;
-        document.getElementById('ticTacToe').rows[row].cells[col].innerHTML = currentPlayer;
-        checkWinner();
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X'; // Switch players
-    }
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+welcomeIn();
+function welcomeIn(){
+  alert("Welcome to TicTacToe");
 }
 
+function makeMove(row, col) {
+  if (board[row][col] === "") {
+    board[row][col] = currentPlayer;
+    document.getElementById("ticTacToe").rows[row].cells[col].innerHTML =
+      currentPlayer;
+      setTimeout(function () {
+        checkWinner();
+    }, 100);
+    currentPlayer = currentPlayer === "X" ? "O" : "X"; // Switch players
+  }
+}
+
+function updateScoreboard(){
+    const scoreboardElement=document.getElementById("scoreboard");
+    scoreboardElement.innerHTML=`
+    Player 1 = ${playerOneWins} | Player 2= ${playerTwoWins} | Draw=${draw}`;
+    resetBoard();
+    
+}
 function checkWinner() {
-    // Check rows, columns, and diagonals for a win
-    for (let i = 0; i < 3; i++) {
-        if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-            alert(`${board[i][0]} wins!`);
-            resetBoard();
-            return;
-        }
-        if (board[0][i] !== '' && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
-            alert(`${board[0][i]} wins!`);
-            resetBoard();
-            return;
-        }
+  // Check rows, columns, and diagonals for a win
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[i][0] !== "" &&
+      board[i][0] === board[i][1] &&
+      board[i][1] === board[i][2]
+    ) {
+      winner=board[i][0];
+      displayWinner();
+      resetBoard();
+      return;
+    }
+    if (
+      board[0][i] !== "" &&
+      board[0][i] === board[1][i] &&
+      board[1][i] === board[2][i]
+    ) {
+        winner=board[0][i];
+        displayWinner();
+        resetBoard();
+      return;
     }
 
-    if (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-        alert(`${board[0][0]} wins!`);
-        resetBoard();
-        return;
-    }
 
-    if (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-        alert(`${board[0][2]} wins!`);
-        resetBoard();
-        return;
-    }
+  if (
+    board[0][0] !== "" &&
+    board[0][0] === board[1][1] &&
+    board[1][1] === board[2][2]
+  ) {
+    winner=board[0][0];
+    displayWinner();
+    resetBoard();
+    return;
+  }
 
-    // Check for a draw
-    if (!board.flat().includes('')) {
-        alert("It's a draw!");
-        resetBoard();
+  if (
+    board[0][2] !== "" &&
+    board[0][2] === board[1][1] &&
+    board[1][1] === board[2][0]
+  ) {
+    winner=board[0][2];
+    displayWinner();
+    resetBoard();
+    return;
+  }
+}
+  // Check for a draw
+  if (!board.flat().includes("")) {
+    winner=0;
+    displayWinner();
+    resetBoard();
+  }
+
+}
+
+function displayWinner(){
+    if(winner==="X"){
+        alert("Player 1 wins");
+        playerOneWins++;
+        updateScoreboard();
     }
+    else if(winner==="O"){
+        alert("Player 2 wins");
+        playerTwoWins++;
+        updateScoreboard();
+       
+    }
+    else if(winner===0){
+        alert("Its a draw");
+        draw++;
+        updateScoreboard();
+        
+    }
+}
+function resetGame(){
+  const scoreboardElement=document.getElementById("scoreboard");
+    scoreboardElement.innerHTML=`
+    Player 1 = 0 | Player 2= 0 | Draw=0`;
+    resetBoard();
+    setTimeout(function () {
+      alert("Game resetted");
+  }, 100);
 }
 
 function resetBoard() {
-    board = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ];
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
 
-    // Clear the table
-    const cells = document.querySelectorAll('td');
-    cells.forEach(cell => cell.innerHTML = '');
-
-    currentPlayer = 'X'; // X starts again
+  // Clear the table
+  const cells = document.querySelectorAll("td");
+  cells.forEach((cell) => (cell.innerHTML = ""));
+  currentPlayer = "X"; // X starts again
 }
